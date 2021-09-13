2619d7912e591b066176aa8b9b0edc6b78ac4ea7
jspearus <jspearus@gmail.com>
2021-09-13 16:36:27 -0500

    buttons added

--- uremote.py
+++ uremote.py
@@ -1,14 +1,33 @@
 from tkinter import *
+#import serial
+import time
+
+#port = serial.Serial("/dev/ttyAMA0", baudrate=115200, timeout=3.0)
 
 root = Tk()
 root.title("uRemote")
 root.geometry('700x400')
-e = Entry(root, width=35, borderwidth=5)
-e.grid(row=0, column=0, columnspan=5, padx=10, pady=10)
 
-btn = Button(root, text="Execute")
+text = ""
+
+
+def Execute():
+    Display("Execute")
+
+
+def Reset():
+    Display("Reset      ")
+
+
+def Display(x):
+    print(x)
+    myLabel = Label(root, text=x)
+    myLabel.place(x=100, y=200)
+
+
+btn = Button(root, text="Execute", command=Execute)
 btn.place(x=600, y=350)
-btn2 = Button(root, text="RESET")
+btn2 = Button(root, text="RESET", command=Reset)
 btn2.place(x=525, y=350)
 
 root.mainloop()
