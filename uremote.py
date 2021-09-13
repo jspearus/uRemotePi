a19d969e245732cdc084e1821326beadbca2a730
jspearus <jspearus@gmail.com>
2021-09-13 16:16:26 -0500

    update

--- uremote.py
+++ uremote.py
@@ -2,12 +2,13 @@ from tkinter import *
 
 root = Tk()
 root.title("uRemote")
+root.geometry('700x400')
 e = Entry(root, width=35, borderwidth=5)
 e.grid(row=0, column=0, columnspan=5, padx=10, pady=10)
 
-root.geometry('700x400')
 btn = Button(root, text="Execute")
-
-btn.grid(column=30, row=100)
+btn.place(x=600, y=350)
+btn2 = Button(root, text="RESET")
+btn2.place(x=525, y=350)
 
 root.mainloop()
