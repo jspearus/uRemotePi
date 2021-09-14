eea8f5258220b558e60b34ded0bee790991cea69
jspearus <jspearus@gmail.com>
2021-09-14 17:01:07 -0500

    added ui features

--- uremote.py
+++ uremote.py
@@ -11,33 +11,44 @@ root.attributes('-fullscreen', True)
 root.configure(background='black')
 root.title("uRemote For All")
 root.geometry('480x640')
+root.config(cursor="none") 
 
 text = ""
 
 
 def Execute():
+    textBox['text'] += "Execute" + "\n"
     Display("Execute")
 
 
 def Reset():
+    textBox['text'] += "Reset" +"\n"
     Display("Reset      ")
 
 
 def Display(x):
-    print(x)
     myLabel = Label(root, text=x)
-    myLabel.place(x=200, y=100)
+    myLabel.place(x=40, y=420)
+
+def Clear():
+    textBox['text'] = ""
+
+textBox = Label(root, bg="blue", width=45, height=20, anchor="nw", justify="left")
+textBox.place(x=40, y=60)
 
 
 osLabel = Label(root, text= "OS Detected: " + platform.system())
-osLabel.place(x=100, y=20)
+osLabel.place(x=40, y=20)
 
 btn = Button(root, text="Execute", command=Execute)
 btn.place(x=350, y=575)
 
 
 btn2 = Button(root, text="RESET", bg="blue", command=Reset)
-btn2.place(x=250, y=575)
+btn2.place(x=175, y=575)
+
+btn4 = Button(root, text="clear", bg="blue", command=Clear)
+btn4.place(x=40, y=575)
 
 
 
