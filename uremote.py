a5fc78b849513a210c36826647872e438729c741
jspearus <35698087+jspearus@users.noreply.github.com>
2021-10-06 14:19:38 -0500

    serialpor on win added

--- uremote.py
+++ uremote.py
@@ -21,13 +21,17 @@ hud.geometry('700x480+800+0')
 text = ""
 
 if platform.system() == "Linux":
-    port = serial.Serial("/dev/ttyAMA0", baudrate=115200, timeout=3.0)
+    port = serial.Serial("/dev/ttyAMA0", baudrate=115200, timeout=10)
     root.config(cursor="none")
     root.attributes('-fullscreen', True)
     hud.attributes('-fullscreen', True)
 elif platform.system() == "Windows":
-    #port = serial.Serial("COM7", baudrate=115200, timeout=3.0)
-    pass
+    try:
+        port = serial.Serial('COM10', baudrate=9600, timeout=10)
+        print("COM10 Connected")
+        port.write(str.encode("comm#"))
+    except serial.SerialException as e:
+        print("device not detected")
 
 
 def Config():
@@ -48,6 +52,7 @@ def Dashboard():
     btn.place(x=700, y=350)
     my_frame.place(x=40, y=60)
     Display("Dashboard                ")
+    port.write(str.encode("Dashboard#"))
 
 
 def Display(x):
@@ -65,6 +70,7 @@ def EXOhud():
     BAT_Stats.place_forget()
     btn5.place(x=550, y=400)
     btn6.place_forget()
+    port.write(str.encode("EXO#"))
 
 
 def BAThud():
@@ -72,6 +78,7 @@ def BAThud():
     EXO_Stats.place_forget()
     btn5.place_forget()
     btn6.place(x=550, y=400)
+    port.write(str.encode("PowerManage#"))
 
 
 def Quit():
