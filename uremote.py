81d3edc2f1bc0a6e4c334e01a4809b68a9bfd1a9
jspearus <35698087+jspearus@users.noreply.github.com>
2022-04-26 18:22:48 -0500

    update ui

--- uremote.py
+++ uremote.py
@@ -1,22 +1,15 @@
 #! /usr/bin/python3
 
 from tkinter import *
+import serial
 import time
 import os
 import platform
 import threading
-import socket
 
 
-HEADER = 64
-PORT = 5000
-FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
-SERVER = "dgscore.ddns.net"
-ADDR = (SERVER, PORT)
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(ADDR)
+
 DataIn = ''
 connected = True
 step = '1000'
@@ -36,47 +29,6 @@ if platform.system() == "Linux":
         print("device not detected")
 
 
-elif platform.system() == "Windows":
-    name_file = 'name.txt'
-
-
-def send(msg):
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b' ' * (HEADER - len(send_length))
-    client.send(send_length)
-    client.send(message)
-    # print(client.recv(2048).decode(FORMAT))
-
-
-def SocketIn():
-    global DataIn
-    global connected
-    print('listening...')
-    while connected:
-        DataIn = client.recv(2048).decode(FORMAT)
-        if not DataIn:
-            break
-        print(DataIn)
-        ####################### COMMANDS ##################
-        if DataIn == 'Rover':
-            roverStatus.config(text='Rover Online')
-        elif DataIn == 'rOffline':
-            roverStatus.config(text='Rover Offline')
-        elif 'Bat:' in DataIn:
-            roverModeD.config(text=DataIn)
-        DataIn = ''
-        time.sleep(.5)
-
-
-with open(name_file) as f:
-    name = f.readline()
-    send(name)
-    print(f"Connected as: {name}")
-    send('site, devices')
-
-
 root = Tk()
 
 
@@ -90,33 +42,14 @@ mynum = 0
 Stop_t = False
 
 
-def navView():
-    Dash_view.place_forget()
-    Pilot_view.place_forget()
-    cam_view.place_forget()
-    Nav_view.place(x=0, y=50)
-
-
-def camView():
-    Dash_view.place_forget()
-    Pilot_view.place_forget()
-    Nav_view.place_forget()
-    cam_view.place(x=0, y=50)
-
-
 def pilotView():
     Dash_view.place_forget()
-    Nav_view.place_forget()
-    cam_view.place_forget()
     Pilot_view.place(x=0, y=50)
 
 
 def Dashboard():
-    send('rover, status')
     Dash_view.place(x=0, y=50)
-    Nav_view.place_forget()
     Pilot_view.place_forget()
-    cam_view.place_forget()
 
 
 def Display(x):
@@ -155,12 +88,41 @@ def sendMove(x):
 
 def Quit():
     global connected
-    send(DISCONNECT_MESSAGE)
-    time.sleep(1)
     connected = False
     global Stop_t
     Stop_t = True
     root.destroy()
+
+def SerialOut(com):
+    port.write(com)
+
+
+def serialRead():
+    global serBuffer
+    serLabel.config(text="Device not Connected...")
+    while True:
+        Data = port.readline()
+        Data = str(Data, 'UTF-8')
+        data = Data.split(',')
+        # serLabel.config(text=data[0])
+        if 'MANTIS' in data[0]:
+            serLabel.config(text="M.A.N.T.I.S. Blade Detected")
+
+        elif 'config' in data[0]:
+            cMode.config(text='Config Mode')
+            CMode.config(text='Config Mode')
+            EXOhud()
+
+        elif 'ctrlblow' in data[0]:
+            alert('Ctrl Bat V Low')
+
+        elif 'alert' in data[0]:
+            alert('Drive Bat V Low')
+        data = ''
+        time.sleep(.2)
+        if Stop_t:
+            break
+
 
 
 ############################ ROOT DISPLAY #################################
@@ -179,17 +141,17 @@ elif platform.system() == "Windows":
 quitBtn = Button(root, text="Quit", bg="DarkOrange2", command=Quit)
 quitBtn.place(x=725, y=10)
 
-pilotBtn = Button(root, height=3, width=10, text="Pilot\nCtrl",
+pilotBtn = Button(root, height=3, width=10, text="Manual\nCtrl",
                   bg="DarkOrange2", command=pilotView)
 pilotBtn.place(x=675, y=100)
 
-camBtn = Button(root, height=3, width=10, text="Vision\nCtrl",
-                bg="DarkOrange2", command=camView)
-camBtn.place(x=675, y=200)
+# camBtn = Button(root, height=3, width=10, text="Vision\nCtrl",
+#                 bg="DarkOrange2", command=camView)
+# camBtn.place(x=675, y=200)
 
-navBtn = Button(root, height=3, width=10,
-                text="Nav\nCtrl", bg="DarkOrange2", command=navView)
-navBtn.place(x=675, y=300)
+# navBtn = Button(root, height=3, width=10,
+#                 text="Nav\nCtrl", bg="DarkOrange2", command=navView)
+# navBtn.place(x=675, y=300)
 
 dashBtn = Button(root, height=3, width=10, text="Dashboard",
                  bg="DarkOrange2", command=Dashboard)
@@ -199,141 +161,64 @@ dashBtn.place(x=675, y=400)
 
 Dash_frame = Frame(root, bg="black")
 
-Dash_view = LabelFrame(root, text="-Dashbord_Johnny-", font=("Arial", 20),
+Dash_view = LabelFrame(root, text="-ANT.ini-Dashboard", font=("Arial", 20),
                        width=650, height=410, bd=5, bg="black", fg="orange")
 Dash_view.place(x=2, y=50)
 
-Pilot_view = LabelFrame(root, text="-Pilot_Ctrl-", font=("Arial", 20),
+Pilot_view = LabelFrame(root, text="-ANT.ini-Manual_Ctrl-", font=("Arial", 20),
                         width=650, height=410, bd=5, bg="black", fg="orange")
-
-cam_view = LabelFrame(root, text="-Vision_Ctrl-", font=("Arial", 20),
-                      width=650, height=410, bd=5, bg="black", fg="orange")
-
-Nav_view = LabelFrame(root, text="-Nav_Ctrl-", font=("Arial", 20),
-                      width=650, height=410, bd=5, bg="black", fg="orange")
 ################ DASHBOARD #####################################
 
-roverStatus = Label(Dash_view, text="Rover Offline",
+roverStatus = Label(Dash_view, text="ANT.ini Offline",
                     bg="red", fg="black", font=("Arial", 25))
 roverStatus.place(x=20, y=15)
 
-roverModeD = Label(Dash_view, text="Mode = Safe", bg="black",
+roverModeD = Label(Dash_view, text="Mode = Sit", bg="black",
                    fg="orange", font=("Arial", 20))
 roverModeD.place(x=20, y=85)
-
-PID_view = LabelFrame(Dash_view, text=" PID_Config ", font=("Arial", 25),
-                      width=250, height=200, bd=5, bg="black", fg="orange")
-PID_view.place(x=300, y=75)
-
-P_val = Label(PID_view, text="P = 3.5", bg="black",
-              fg="white", font=("Arial", 20))
-P_val.place(x=8, y=20)
-I_val = Label(PID_view, text="I = 1.0", bg="black",
-              fg="white", font=("Arial", 20))
-I_val.place(x=8, y=65)
-D_val = Label(PID_view, text="D = 0", bg="black",
-              fg="white", font=("Arial", 20))
-D_val.place(x=8, y=110)
-
-################ NAV Ctrl ###############################
-
-roverSpeed = Label(Nav_view, text="Mode = Safe", bg="black",
-                   fg="white", font=("Arial", 20))
-roverSpeed.place(x=50, y=20)
-
-scanBtn = Button(Nav_view, text="Scan\nAll", height=3,
-                 width=10, bg="orange", fg="black", font=("Arial", 10), command=lambda: sendMove(f'rover, scan'))
-scanBtn.place(x=525, y=100)
-
-scanEnBtn = Button(Nav_view, text="Enable", height=3,
-                   width=10, bg="orange", fg="black", font=("Arial", 10), command=lambda: send('rover, eSonar'))
-scanEnBtn.place(x=525, y=200)
-
-scanDisBtn = Button(Nav_view, text="Disable", height=3,
-                    width=10, bg="orange", fg="black", font=("Arial", 10), command=lambda: send('rover, dSonar'))
-scanDisBtn.place(x=525, y=300)
-
-################# VISION VIEW ########################################
-camSpeed = Label(cam_view, text=f"Speed = MED", bg="black",
-                 fg="white", font=("Arial", 15))
-camSpeed.place(x=475, y=20)
-cLowBtn = Button(cam_view, text="LOW", height=2,
-                 width=5, bg="orange", fg="black", font=("Arial", 8), command=lambda: setCamSpeed(f'rover, cam-clow'))
-cLowBtn.place(x=430, y=70)
-cMedBtn = Button(cam_view, text="MED", height=2,
-                 width=5, bg="orange", fg="black", font=("Arial", 8), command=lambda: setCamSpeed(f'rover, cam-cmed'))
-cMedBtn.place(x=500, y=70)
-cHiBtn = Button(cam_view, text="HI", height=2,
-                width=5, bg="orange", fg="black", font=("Arial", 8), command=lambda: setCamSpeed(f'rover, cam-chi'))
-cHiBtn.place(x=570, y=70)
-
-cUpBtn = Button(cam_view, text="Up", height=2,
-                width=5, bg="orange", fg="black", font=("Arial", 10), command=lambda: send(f'rover, cam-cup'))
-cUpBtn.place(x=475, y=190)
-cdnBtn = Button(cam_view, text="Down", height=2,
-                width=5, bg="orange", fg="black", font=("Arial", 10), command=lambda: send(f'rover, cam-cdown'))
-cdnBtn.place(x=475, y=310)
-cLfBtn = Button(cam_view, text="Left", height=2,
-                width=5, bg="orange", fg="black", font=("Arial", 10), command=lambda: send(f'rover, cam-cleft'))
-cLfBtn.place(x=410, y=250)
-cRtBtn = Button(cam_view, text="Right", height=2,
-                width=5, bg="orange", fg="black", font=("Arial", 10), command=lambda: send(f'rover, cam-cright'))
-cRtBtn.place(x=540, y=250)
-
-cLeftBtn = Button(cam_view, text="Look\nLeft", height=2,
-                  width=5, bg="orange", fg="black", font=("Arial", 15), command=lambda: send(f'rover, cam-pLeft'))
-cLeftBtn.place(x=25, y=300)
-cFrontBtn = Button(cam_view, text="Look\nFront", height=2,
-                   width=5, bg="orange", fg="black", font=("Arial", 15), command=lambda: send(f'rover, cam-pFront'))
-cFrontBtn.place(x=125, y=300)
-cRightBtn = Button(cam_view, text="Look\nRight", height=2,
-                   width=5, bg="orange", fg="black", font=("Arial", 15), command=lambda: send(f'rover, cam-pRight'))
-cRightBtn.place(x=225, y=300)
 
 
 ################# PILOT VIEW ########################################
 
-roverSpeed = Label(Pilot_view, text=f"Speed = {step}", bg="black",
-                   fg="white", font=("Arial", 15))
-roverSpeed.place(x=200, y=115)
+# roverSpeed = Label(Pilot_view, text=f"Speed = {step}", bg="black",
+#                    fg="white", font=("Arial", 15))
+# roverSpeed.place(x=200, y=115)
 
 pUpBtn = Button(Pilot_view, text="Forward", height=2,
-                width=5, bg="orange", fg="black", font=("Arial", 10), command=lambda: sendMove(f'rover, move-f-'))
+                width=5, bg="orange", fg="black", font=("Arial", 10), command=lambda: SerialOut(b"stepf#"))
 pUpBtn.place(x=130, y=170)
 pdnBtn = Button(Pilot_view, text="Back", height=2,
-                width=5, bg="orange", fg="black", font=("Arial", 10), command=lambda: sendMove(f'rover, move-b-'))
+                width=5, bg="orange", fg="black", font=("Arial", 10), command=lambda: SerialOut(b"stepb#"))
 pdnBtn.place(x=130, y=290)
 pLfBtn = Button(Pilot_view, text="Left", height=2,
-                width=5, bg="orange", fg="black", font=("Arial", 10), command=lambda: sendMove(f'rover, move-l-'))
+                width=5, bg="orange", fg="black", font=("Arial", 10), command=lambda: SerialOut(b"stepl#"))
 pLfBtn.place(x=65, y=230)
 pRtBtn = Button(Pilot_view, text="Right", height=2,
-                width=5, bg="orange", fg="black", font=("Arial", 10), command=lambda: sendMove(f'rover, move-r-'))
+                width=5, bg="orange", fg="black", font=("Arial", 10), command=lambda: SerialOut(b"stepr#"))
 pRtBtn.place(x=195, y=230)
 
-pLeftBtn = Button(Pilot_view, text="Rotate\nLeft", height=2,
-                  width=5, bg="orange", fg="black", font=("Arial", 10), command=lambda: sendMove(f'rover, move-rl-'))
+pLeftBtn = Button(Pilot_view, text="Stand", height=2,
+                  width=5, bg="orange", fg="black", font=("Arial", 10), command=lambda: SerialOut(b"stand#"))
 pLeftBtn.place(x=20, y=320)
-pFrontBtn = Button(Pilot_view, text="Rotate\nRight", height=2,
-                   width=5, bg="orange", fg="black", font=("Arial", 10), command=lambda: sendMove(f'rover, move-rr-'))
+pFrontBtn = Button(Pilot_view, text="Sit", height=2,
+                   width=5, bg="orange", fg="black", font=("Arial", 10), command=lambda: SerialOut(b"sit#"))
 pFrontBtn.place(x=230, y=320)
-pStopBtn = Button(Pilot_view, text="STOP", height=2,
-                  width=5, bg="orange", fg="black", font=("Arial", 10), command=lambda: sendMove(f'rover, move-s-s'))
+pStopBtn = Button(Pilot_view, text="Attack", height=2,
+                  width=5, bg="orange", fg="black", font=("Arial", 10), command=lambda: SerialOut(b"attack#"))
 pStopBtn.place(x=550, y=320)
 
-pseedLabel = Label(Pilot_view, text="1000                    Speed Select                    13000", bg="black",
-                   fg="white", font=("Arial", 10))
-pseedLabel.place(x=20, y=0)
-pspeed = Scale(Pilot_view, bg="orange", fg="black",
-               font=("Arial", 10), from_=1000, to=13000, tickinterval=3000, width=35, length=300, orient=HORIZONTAL)
-pspeed.place(x=20, y=25)
-setBtn = Button(Pilot_view, text="Set", height=1,
-                width=5, bg="orange", fg="black", font=("Arial", 10), command=lambda: setSpeed(f'{pspeed.get()}'))
-setBtn.place(x=25, y=125)
+# pseedLabel = Label(Pilot_view, text="1000                    Speed Select                    13000", bg="black",
+#                    fg="white", font=("Arial", 10))
+# pseedLabel.place(x=20, y=0)
+# pspeed = Scale(Pilot_view, bg="orange", fg="black",
+#                font=("Arial", 10), from_=1000, to=13000, tickinterval=3000, width=35, length=300, orient=HORIZONTAL)
+# pspeed.place(x=20, y=25)
+# setBtn = Button(Pilot_view, text="Set", height=1,
+#                 width=5, bg="orange", fg="black", font=("Arial", 10), command=lambda: SerialOut(f'{pspeed.get()}'))
+# setBtn.place(x=25, y=125)
 
-SockThread = threading.Thread(target=SocketIn, args=())
-SockThread.setDaemon(True)
-SockThread.start()
-
-send(name)
+serial = threading.Thread(target=serialRead, args=())
+serial.setDaemon(True)
+serial.start()
 
 root.mainloop()
